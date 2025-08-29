fb4439beec3622e8bf98a3435135bf90babda7e4
raihan9099 <140356472+raihan9099@users.noreply.github.com>
2025-08-29 15:34:32 +0600

    Update uid2.js

--- scripts/cmds/uid2.js
+++ scripts/cmds/uid2.js
@@ -1,6 +1,7 @@
 const { GoatWrapper } = require("fca-liane-utils"); module.exports = {
   config: {
-    name: "uid",
+    name: "uid2",
+    aliases: ["uid", "ucontact"]
     version: "1.0.0",
     permission: 0,
     credits: "XNIL",
