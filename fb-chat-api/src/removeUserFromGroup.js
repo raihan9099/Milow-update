cb744613e88aa11bc5d959e4901dd2c44cce6cdb
raihan9099 <140356472+raihan9099@users.noreply.github.com>
2025-08-29 11:37:26 +0600

    Update removeUserFromGroup.js

--- fb-chat-api/src/removeUserFromGroup.js
+++ fb-chat-api/src/removeUserFromGroup.js
@@ -4,7 +4,7 @@ var utils = require("../utils");
 var log = require("npmlog");
 
 module.exports = function (defaultFuncs, api, ctx) {
-  return function removeUserFromGroup(userID,) {
+  return function removeUserFromGroup(userID, threadID, callback) {
     if (!callback && (utils.getType(threadID) === "Function" || utils.getType(threadID) === "AsyncFunction")) throw { error: "please pass a threadID as a second argument." };
     if (utils.getType(threadID) !== "Number" && utils.getType(threadID) !== "String") throw { error: "threadID should be of type Number or String and not " + utils.getType(threadID) + "." };
     if (utils.getType(userID) !== "Number" && utils.getType(userID) !== "String") throw { error: "userID should be of type Number or String and not " + utils.getType(userID) + "." };
