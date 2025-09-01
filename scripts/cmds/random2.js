ae42762de9d19a04b38e57671aff29f5aa34d1da
raihan9099 <140356472+raihan9099@users.noreply.github.com>
2025-09-01 08:18:11 +0600

    Create random2.js

---
+++ scripts/cmds/random2.js
@@ -0,0 +1,49 @@
+const request = require("request");
+const fs = require("fs");
+const axios = require("axios");
+
+module.exports = {
+  config: {
+    name: "randomvid",
+    version: "0.0.2",
+    permission: 0,
+    credits: "Nisan",
+    description: "rndm video",
+    category: "user",
+    usages: "[name]",
+    cooldowns: 5,
+  },
+  onStart: async function ({ api, message, args }) {
+    const nameParam = args.join(" ");
+
+    if (!nameParam) {
+      return message.reply(`[ ! ] Input Name.\nEx: ${global.GoatBot.config.prefix}rndm nayan`);
+    }
+
+    try {
+      const apis = await axios.get('https://raw.githubusercontent.com/MOHAMMAD-NAYAN-07/Nayan/main/api.json');
+      const n = apis.data.api;
+      const res = await axios.get(`${n}/random?name=${encodeURIComponent(nameParam)}`);
+
+      const videoUrl = res.data.data.url;
+      const name = res.data.data.name;
+      const cp = res.data.data.cp;
+      const ln = res.data.data.length;
+      const filePath = `${__dirname}/cache/video.mp4`;
+
+      const file = fs.createWriteStream(filePath);
+      request(videoUrl)
+        .pipe(file)
+        .on("close", () => {
+          message.reply({
+            body: `${cp}\n\n𝐓𝐨𝐭𝐚𝐥 𝐕𝐢𝐝𝐞𝐨𝐬: [${ln}]\n𝐀𝐝𝐝𝐞𝐝 𝐓𝐡𝐢𝐬 𝐕𝐢𝐝𝐞𝐨 𝐓𝐨 𝐓𝐡𝐞 𝐀𝐩𝐢 𝐁𝐲 [${name}]`,
+            attachment: fs.createReadStream(filePath)
+          }, () => fs.unlinkSync(filePath));
+        });
+
+    } catch (err) {
+      console.error(err);
+      return message.reply("Something went wrong. Please try again later.");
+    }
+  }
+};
